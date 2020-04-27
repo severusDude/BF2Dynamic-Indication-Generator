4d08cc4da0ee6b9a49b6c5d56b44310c4dc265cc
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-28 06:49:15 +0700

    Added user input tracking

--- GenerateMain.py
+++ GenerateMain.py
@@ -9,6 +9,45 @@ class ControlMainWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.main_ui = UserInterface()
         self.main_ui.setupUi(self)
+        self.wep_index = int()
+        self.wep_name = str()
+        self.wep_indi = str()
+
+        # send input values into declared variables
+        self.main_ui.weapon_index_input.textChanged.connect(
+            self.weapon_index_act)
+        self.main_ui.weapon_name_input.textChanged.connect(
+            self.weapon_name_act)
+        self.main_ui.weapon_indi_input.textChanged.connect(
+            self.weapon_indi_act)
+
+    # get weapon index from user input
+    def weapon_index_act(self, value):
+        self.main_ui.gen_button.setDisabled(False)
+        if len(value) > 0:
+            self.wep_index = value
+            self.wep_index = int(self.wep_index)
+            print(self.wep_index)
+        else:
+            None
+
+    # get weapon name from user input
+    def weapon_name_act(self, value):
+        self.main_ui.gen_button.setDisabled(False)
+        if len(value) > 0:
+            self.wep_name = value
+            print(self.wep_name)
+        else:
+            None
+
+    # get weapon indication from user input
+    def weapon_indi_act(self, value):
+        self.main_ui.gen_button.setDisabled(False)
+        if len(value) > 0:
+            self.wep_indi = value
+            print(self.wep_indi)
+        else:
+            None
 
     # defining system exception
     def log_uncaught_exceptions(self, ex_cls, ex, tb):
