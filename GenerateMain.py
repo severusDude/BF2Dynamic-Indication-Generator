c3c2281071e203d600e5b15d673df79970312f04
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-28 21:25:04 +0700

    Try to separate generate dictionary from the main control

--- GenerateMain.py
+++ GenerateMain.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Generate import *
 from generateindication import *
+from generatedictionary import *
 import qdarkstyle
 import sys
 
@@ -11,6 +12,7 @@ class ControlMainWindow(QtWidgets.QMainWindow):
         self.main_ui = UserInterface()
         self.main_ui.setupUi(self)
         self.gen_indi = GenerateIndication()
+        self.gen_dict = GenerateDictionary()
         self.wep_index = int()
         self.wep_name = str()
         self.wep_indi = str()
@@ -28,45 +30,36 @@ class ControlMainWindow(QtWidgets.QMainWindow):
     def gen_button_act(self):
         if self.wep_index and len(self.wep_name) and len(self.wep_indi) > 0:
             self.gen_indi.init(self.wep_index)
+            self.gen_dict.init(self.wep_name, self.wep_index, self.wep_indi)
         else:
             self.wep_index = 161
             self.wep_name = "USRIF_M4"
             self.wep_indi = "M4"
             self.gen_indi.init(self.wep_index)
+            self.gen_dict.init(self.wep_name, self.wep_index, self.wep_indi)
             # QtWidgets.QMessageBox.warning(
             #     self, "Error", "You need to input all the textfield")
 
-    def weapon_dict_manage(self, index, name, indi):
-        try:
-            with open('game\\weapons.py', 'r+') as f:
-                # print(f.readlines())
-                x = self.weapon_dict_add(f, index, name, indi)
-                f.write(x)
-        except FileNotFoundError as e:
-            print(f"Required dictionary doesn't exist {e}")
-        finally:
-            f.close()
+    # def weapon_dict_add(self, file, index, name, indi):
+    #     contents = file.readlines()
+    #     text_line, req_index = self.find_dict_place(contents, index)
+    #     print(text_line)
+    #     x = self.line_num_for_phrase_in_file(text_line, file)
+    #     print(x)
 
-    def weapon_dict_add(self, file, index, name, indi):
-        contents = file.readlines()
-        text_line, req_index = self.find_dict_place(contents, index)
-        print(text_line)
-        x = self.line_num_for_phrase_in_file(text_line, file)
-        print(x)
+    #     req_index = int(req_index)
+    #     contents.insert(req_index,
+    #                     f"		\"{name}\"			 : {index},		 # {indi}")
+    #     contents = "".join(contents)
+    #     return contents
 
-        req_index = int(req_index)
-        contents.insert(req_index,
-                        f"		\"{name}\"			 : {index},		 # {indi}")
-        contents = "".join(contents)
-        return contents
-
-    def find_dict_place(self, file, index):
-        for num, line in enumerate(file, 1):
-            index_str = index - 1
-            index_str = str(index_str)
-            if index_str in line:
-                print(f"it here {index_str}")
-                return line, index_str
+    # def find_dict_place(self, file, index):
+    #     for num, line in enumerate(file, 1):
+    #         index_str = index - 1
+    #         index_str = str(index_str)
+    #         if index_str in line:
+    #             print(f"it here {index_str}")
+    #             return line, index_str
 
     # get weapon index from user input
     def weapon_index_act(self, value):
