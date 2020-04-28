5fb0b2a7a87de5597cfa7c0689133ef2f9d38843
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-29 04:29:14 +0700

    Working generate dictionary

--- generatedictionary.py
+++ generatedictionary.py
@@ -8,8 +8,7 @@ class GenerateDictionary:
     def open_dict(self):
         try:
             with open('game\\weapons.py', 'r+') as f:
-                print(f.readlines())
-                self.weapon_dict_add(f, self.index, self.name, self.indi)
+                self.dict_add(f, self.index, self.name, self.indi)
         except FileNotFoundError as e:
             print(f"Dictionary file doesn't exist {e}")
         finally:
@@ -45,18 +44,19 @@ class GenerateDictionary:
 
     #     return -1
 
-    def weapon_dict_add(self, file, index, name, indi):
+    def dict_add(self, file, index, name, indi):
         contents = file.readlines()
-        text_line, req_index, line_num = self.find_dict_place(contents, index)
-        print(text_line)
+        text_line, req_index, line_num = self.find_dict_loc(contents, index)
+        print(line_num)
 
         req_index = int(req_index)
         contents.insert(line_num,
-                        f"		\"{name}\"			 : {index},		 # {indi}")
+                        f"		\"{name}\"			 : {index},		 # {indi}\n")
         contents = "".join(contents)
+        file.write(contents)
         return contents
 
-    def find_dict_place(self, file, index):
+    def find_dict_loc(self, file, index):
         index_str = index - 1
         index_str = str(index_str)
         for num, line in enumerate(file, 1):
@@ -66,4 +66,4 @@ class GenerateDictionary:
 
 
 x = GenerateDictionary()
-x.init("usrif_m4", 161, "M4")
+x.init("bf4_scar_sv", 16, "SCAR SV")
