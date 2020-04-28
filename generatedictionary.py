760b1eda36352b38a4681b76f05f9a9067957495
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-29 04:41:07 +0700

    Remove old code and improved string

--- generatedictionary.py
+++ generatedictionary.py
@@ -14,36 +14,6 @@ class GenerateDictionary:
         finally:
             f.close()
 
-    # def dict_act(self, name, indi, wep_index):
-    #     try:
-    #         with open('game\\weapons.py', 'r+') as f:
-    #             print(f.readlines())
-    #             self.dict_add(wep_index, f)
-    #     except FileNotFoundError as e:
-    #         print(f"Dictionary file doesn't exist {e}")
-    #         return False
-    #     finally:
-    #         f.close()
-
-    # def dict_add(self, key, file):
-    #     contents = file.readlines()
-    #     text_line, index_line = self.find_dict_loc(key, contents)
-    #     print(f"text line type is {type(text_line)} and index {text_line}")
-    #     print(f"text line type is {type(index_line)} and index {index_line}")
-
-    # def find_dict_loc(self, dict_item, file):
-    #     dict_item -= 1
-    #     print("step 1")
-    #     for num_line, line in enumerate(file):
-    #         print("step 2")
-    #         if dict_item in line:
-    #             print(
-    #                 f"dict_item contain {dict_item} is on line number {num_line}")
-    #             print(line)
-    #             return line, num_line
-
-    #     return -1
-
     def dict_add(self, file, index, name, indi):
         contents = file.readlines()
         text_line, req_index, line_num = self.find_dict_loc(contents, index)
@@ -51,7 +21,7 @@ class GenerateDictionary:
 
         req_index = int(req_index)
         contents.insert(line_num,
-                        f"		\"{name}\"			 : {index},		 # {indi}\n")
+                        f"\n		\"{name}\"			 : {index},		 # {indi}\n")
         contents = "".join(contents)
         file.write(contents)
         return contents
