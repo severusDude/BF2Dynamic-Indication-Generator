a8c42ca9bfde0c4021edb1c423b8cf1328f391da
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-29 07:21:16 +0700

    Change window title

--- Generate.py
+++ Generate.py
@@ -5,7 +5,7 @@ class UserInterface(QtWidgets.QMainWindow):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(500, 450)
-        MainWindow.setWindowTitle("Generatea")
+        MainWindow.setWindowTitle("Crypter")
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.only_int = QtGui.QIntValidator()
