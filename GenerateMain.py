8cb4dea63099b131fd82e22df84703385ec031cf
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-29 12:44:51 +0700

    Add checking file before generate

--- GenerateMain.py
+++ GenerateMain.py
@@ -4,6 +4,7 @@ from generateindication import *
 from generatedictionary import *
 import qdarkstyle
 import sys
+import os
 
 
 class ControlMainWindow(QtWidgets.QMainWindow):
@@ -16,6 +17,9 @@ class ControlMainWindow(QtWidgets.QMainWindow):
         self.wep_index = int()
         self.wep_name = str()
         self.wep_indi = str()
+        self.f1_status = bool()
+        self.f2_status = bool()
+        self.f3_status = bool()
 
         # send input values into declared variables
         self.main_ui.weapon_index_input.textChanged.connect(
@@ -25,15 +29,54 @@ class ControlMainWindow(QtWidgets.QMainWindow):
         self.main_ui.weapon_indi_input.textChanged.connect(
             self.weapon_indi_act)
 
+        # connect gen_button to gen_button_act function
         self.main_ui.gen_button.clicked.connect(self.gen_button_act)
 
     def gen_button_act(self):
         if self.wep_index and len(self.wep_name) and len(self.wep_indi) > 0:
-            self.gen_indi.init(self.wep_index)
-            self.gen_dict.init(self.wep_name, self.wep_index, self.wep_indi)
+            # over complicated lol
+            att_wep = "HUD\\HudSetup\\Killtext\\HudElementsAttackerWeapon.con"
+            wep_dict = "game\\weapons.py"
+            read_file = 'r+'
+            for page in range(1, 7):
+                if self.find_file(f'HUD\\HudSetup\\Killtext\\HudElementsIndication{page}.con', read_file):
+                    self.f1_status = True
+                else:
+                    self.f1_status = False
+                    QtWidgets.QMessageBox.warning(
+                        self, "Error 1", f"Error code: 1\nIndication{page} files not found")
+
+            if self.find_file(att_wep, read_file):
+                self.f2_status = True
+            else:
+                self.f2_status = False
+                QtWidgets.QMessageBox.warning(
+                    self, "Error 2", f"Error code: 2\nAttackerWeapon file not found")
+
+            if self.find_file(wep_dict, read_file):
+                self.f3_status = True
+            else:
+                self.f3_status = False
+                QtWidgets.QMessageBox.warning(
+                    self, "Error 3", "Error code: 3\nweapons.py file not found")
+
+            if self.f1_status == True and self.f2_status == True and self.f3_status == True:
+                self.gen_indi.init(self.wep_index)
+                self.gen_dict.init(
+                    self.wep_name, self.wep_index, self.wep_indi)
+
         else:
             QtWidgets.QMessageBox.warning(
                 self, "Error", "You need to input all the textfield")
+
+    def find_file(self, file, open_mode):
+        try:
+            with open(file, open_mode) as file_read:
+                return True
+        except FileNotFoundError as e:
+            print(
+                f"Exception FileNotFoundError\nOpened file: {file}\nOriginal: {e}")
+            return False
 
     # get weapon index from user input
     def weapon_index_act(self, value):
