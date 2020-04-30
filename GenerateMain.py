e13c2858b5d871f0ceb59939b9aeebe37eda27b4
severusDude <60343449+severusDude@users.noreply.github.com>
2020-05-01 01:08:42 +0700

    Added automatic backup and improved indication files checking

--- GenerateMain.py
+++ GenerateMain.py
@@ -2,6 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from Generate import *
 from generateindication import *
 from generatedictionary import *
+from backup import *
 import qdarkstyle
 import sys
 import os
@@ -18,9 +19,12 @@ class ControlMainWindow(QtWidgets.QMainWindow):
         self.wep_name = str()
         self.wep_indi = str()
         self.f1_status = bool()
-        self.f1_status_1 = bool()
         self.f2_status = bool()
         self.f3_status = bool()
+        self.all_fstatus = bool()
+        self.backup_path = 'backups'
+        self.indi_path = 'HUD\\HudSetup\\Killtext'
+        self.dict_path = 'game'
 
         # send input values into declared variables
         self.main_ui.weapon_index_input.textChanged.connect(
@@ -36,13 +40,19 @@ class ControlMainWindow(QtWidgets.QMainWindow):
 
     def gen_button_act(self):
         if self.wep_index and len(self.wep_name) and len(self.wep_indi) > 0:
+
             self.start_check()
 
             # action if all required file is checked and exist
-            if self.f1_status_1 == True and self.f2_status == True and self.f3_status == True:
-                self.gen_indi.init(self.wep_index)
-                self.gen_dict.init(
-                    self.wep_name, self.wep_index, self.wep_indi)
+            if self.f1_status == True and self.f2_status == True and self.f3_status == True:
+                limit = 5
+                if BackupFiles(self.indi_path, self.dict_path, limit).compress_succes:
+                    self.gen_indi.init(self.wep_index)
+                    self.gen_dict.init(
+                        self.wep_name, self.wep_index, self.wep_indi)
+                else:
+                    QtWidgets.QMessageBox.critical(
+                        self, "CAUTION", "Automatic backup system is failed to backup.\nFurther generating scripts is cancelled")
 
         else:
             QtWidgets.QMessageBox.warning(
@@ -65,21 +75,16 @@ class ControlMainWindow(QtWidgets.QMainWindow):
         for page in range(index_start, index_end):
             indi_files = f'HUD\\HudSetup\\Killtext\\HudElementsIndication{page}.con'
             if self.check_file(indi_files, read_file):
-                self.f1_status = True
-            else:
-                self.f1_status = False
-                QtWidgets.QMessageBox.critical(
-                    self, "Error FNE_A_1", f"Error code: A_{page}\n\nRequired file is missing:\n\tHUD\\HudSetup\\Killtext\\HudElementsIndication{page}.con")
-
-            if self.f1_status:
                 index_list.append(page)
             else:
                 print(f"error at HudElementsIndication{page}.con")
+                QtWidgets.QMessageBox.critical(
+                    self, "Error FNE_A_1", f"Error code: A_{page}\n\nRequired file is missing:\n\tHUD\\HudSetup\\Killtext\\HudElementsIndication{page}.con")
 
         if index_list == index_listed:
-            self.f1_status_1 = True
+            self.f1_status = True
         else:
-            self.f1_status_1 = False
+            self.f1_status = False
 
         # check HudElementsAttackerWeapon.con
         if self.check_file(att_wep, read_file):
