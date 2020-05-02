977fa1c8e7667df86260632ac7e3d499fac1386e
severusDude <60343449+severusDude@users.noreply.github.com>
2020-05-02 12:30:04 +0700

    now work alongside with app

--- batch.py
+++ batch.py
@@ -5,16 +5,14 @@ import re
 
 
 class BatchProcessing:
-    def __init__(self, file_path):
-        self.FILE_PATH = file_path
+    def __init__(self, file_contents, active):
         self.NAME_PATTERN = r'\[.*?\]'
         self.KEY_PATTERN = r'\(.*?\)'
         self.OPEN_MODE = 'r'
-        self.ACTIVATION_KEY = "activate"
         self.REMOVE_CHAR = ["[", "]", "(", ")"]
+        self.ACTIVE = active
+        self.file_contents = file_contents
         self.file_exist = bool()
-        self.is_active = bool()
-        self.file_contents = list()
         self.name_items = list()
         self.key_items = list()
 
@@ -25,48 +23,15 @@ class BatchProcessing:
     # start the whole system
     def init(self):
 
-        self.find_file()
+        # self.find_file()
 
-        if self.file_exist:
-            print("succes, file is exist")
-            if self.is_active:
-                print("file meet requirment")
-                self.find_name()
-                self.find_key()
-            else:
-                print("file not meet requirment")
-
+        if self.ACTIVE:
+            print("file meet requirment")
+            self.find_name()
+            self.find_key()
         else:
-            print("failed, file not exist")
+            print("file not meet requirment")
 
-    # check the inputed file was exist
-    def find_file(self):
-        if os.path.exists(self.FILE_PATH):
-            self.file_exist = True
-
-            with open(self.FILE_PATH, self.OPEN_MODE) as f:
-                self.check_file(f.readlines())
-
-        else:
-            self.file_exist = False
-
-    # check if file meet requirments
-    def check_file(self, contents):
-        print(contents)
-        self.file_contents = contents
-        key = self.file_contents[0]
-        key = key.replace('\n', '')
-        print(self.file_contents)
-
-        if key == self.ACTIVATION_KEY:
-            self.is_active = True
-            print("batch file is active")
-
-        else:
-            self.is_active = False
-            print("batch file is passive")
-
-    # find indication name
     def find_name(self):
         for item in self.file_contents:
             item = item.replace('\n', '')
@@ -102,5 +67,5 @@ class BatchProcessing:
                 print(item)
 
 
-x = BatchProcessing('batch\\batch-test.txt')
-print(x)
+# x = BatchProcessing('batch\\batch-test.txt')
+# print(x)
