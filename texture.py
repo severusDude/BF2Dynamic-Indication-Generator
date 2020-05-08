e5d07e5abeb164b28f0709ce6b0e98856a4e5373
severusDude <60343449+severusDude@users.noreply.github.com>
2020-05-08 08:12:13 +0700

    added conversion to .dds

--- texture.py
+++ texture.py
@@ -1,16 +1,24 @@
-from win32com.client import Dispatch
 import pathlib
 import os
+
+from win32com.client import Dispatch
+from wand.image import Image
 
 APP = "Photoshop.Application"
 EXPORT_AS = "Photoshop.ExportOptionsSaveForWeb"
 
+# path
 CURRENT_DIR = str(pathlib.Path(__file__).parent.absolute())
+EXPORT_PATH = f"{CURRENT_DIR}\\export"
+PNG_PATH = f"{CURRENT_DIR}\\export\\png"
+DDS_PATH = f"{CURRENT_DIR}\\export\\dds"
 
-print(CURRENT_DIR)
-
-if not os.path.exists(f"{CURRENT_DIR}\\export"):
-    os.makedirs(f"{CURRENT_DIR}\\export")
+if not os.path.exists(EXPORT_PATH):
+    os.makedirs(EXPORT_PATH)
+    if not os.path.exists(PNG_PATH):
+        os.makedirs(PNG_PATH)
+    if not os.path.exists(DDS_PATH):
+        os.makedirs(DDS_PATH)
 
 INDI_PATH = f"psd\\Indicationweapon.psd"
 KILLED_PATH = f"psd\\KilledIndicationWeapon.psd"
@@ -54,16 +62,21 @@ class GenerateTextureSingle:
         shade_text.contents = f"[{self.INDI.upper()}]"
         main_text.contents = f"[{self.INDI.upper()}]"
 
-        export_png = f"{CURRENT_DIR}\\export\\Indicationweapon{self.INDEX}.png"
+        export_png = f"{CURRENT_DIR}\\export\\png\\Indicationweapon{self.INDEX}.png"
+        export_dds = f"{CURRENT_DIR}\\export\\dds\\Indicationweapon{self.INDEX}.dds"
 
         doc.Export(ExportIn=export_png, ExportAs=2, Options=OPTIONS)
+
+        with Image(filename=export_png) as img:
+            img.compression = 'dxt5'
+            img.save(filename=export_dds)
 
     def generate_killed(self, doc):
         pass
 
 
 class GenerateTextureBatch:
-    def __init__(self, wep_list, index):
+    def __init__(self, index, wep_list):
         self.WEP_LIST = wep_list
         self.INDEX = index
         self.LAST_INDEX = len(self.WEP_LIST) + self.INDEX
@@ -96,9 +109,14 @@ class GenerateTextureBatch:
             shade_text.contents = f"[{item.upper()}]"
             main_text.contents = f"[{item.upper()}]"
 
-            export_png = f"{CURRENT_DIR}\\export\\Indicationweapon{fileindex}.png"
+            export_png = f"{CURRENT_DIR}\\export\\png\\Indicationweapon{fileindex}.png"
+            export_dds = f"{CURRENT_DIR}\\export\\dds\\Indicationweapon{fileindex}.dds"
 
             doc.Export(ExportIn=export_png, ExportAs=2, Options=OPTIONS)
+
+            with Image(filename=export_png) as img:
+                img.compression = 'dxt5'
+                img.save(filename=export_dds)
 
     def generate_killed(self, doc):
         pass
